5237b57065d2a16cfb26fa0509729cdf0ee4009e
mohamedelshamy55 <mohamedelshamy55@gmail.com>
2022-03-24 02:16:27 +0200

    Update project files

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,40 +2,34 @@ require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
   describe 'GET /index' do
-    before(:each) do
-      get '/users'
+    before(:example) { get users_path }
+
+    it 'should be a success' do
+      expect(response).to have_http_status(:ok)
     end
 
-    it 'renders correct placeholder text ' do
-      expect(response.body).to include('Users index page')
+    it "should render 'index' template" do
+      expect(response).to render_template('index')
     end
 
-    it 'renders correct template' do
-      expect(response).to render_template(:index)
-    end
-
-    it "'http request is successfull" do
-      expect(response).to have_http_status(:success)
+    it 'should return the correct placeholder text' do
+      expect(response.body).to include('Here shows list of users and their posts')
     end
   end
-end
 
-RSpec.describe 'Users', type: :request do
   describe 'GET /show' do
-    before(:each) do
-      get '/users/:id'
+    before(:example) { get user_path(1) }
+
+    it 'should be a success' do
+      expect(response).to have_http_status(:ok)
     end
 
-    it 'renders correct placeholder text ' do
-      expect(response.body).to include('Users show page')
+    it "should render 'show' template" do
+      expect(response).to render_template('show')
     end
 
-    it 'renders correct template' do
-      expect(response).to render_template(:show)
-    end
-
-    it "'http request is successfull" do
-      expect(response).to have_http_status(:success)
+    it 'should return the correct placeholder text' do
+      expect(response.body).to include('Here is a particular user')
     end
   end
 end
