ac1bfd8b3e15ba9e21af66f6809aacacd4b9159d
mohamedelshamy55 <mohamedelshamy55@gmail.com>
2022-03-29 00:45:33 +0200

    Update project files

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
-  get 'users/:user_id/posts' => 'posts#index', as: 'user_posts'
-  get 'users/:user_id/posts/:id' => 'posts#show', as: 'user_post'
-  get 'users' => 'users#index', as: 'users'
-  get 'users/:id' => 'users#show', as: 'user'
+  # get 'users/:user_id/posts' => 'posts#index', as: 'user_posts'
+  # get 'users/:user_id/posts/:id' => 'posts#show', as: 'user_post'
+  # get 'users' => 'users#index', as: 'users'
+  # get 'users/:id' => 'users#show', as: 'user'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  root 'users#index'
+  resources :users, only: %i[index show] do
+    resources :posts, only: %i[index show]
+  end
   # Defines the root path route ("/")
   # root "articles#index"
 end
