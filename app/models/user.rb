63b602fe779163fed35581a5810009e31c28b7de
mohamedelshamy55 <mohamedelshamy55@gmail.com>
2022-03-31 03:15:29 +0200

    Update project files

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,13 @@
 class User < ApplicationRecord
-  has_many :posts, dependent: :destroy, foreign_key: 'author_id'
-  has_many :comments, dependent: :destroy, foreign_key: 'author_id'
-  has_many :likes, dependent: :destroy, foreign_key: 'author_id'
+  validates :name, presence: true
+  validates :bio, presence: true
+  validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  has_many :posts, foreign_key: 'author_id'
+  has_many :comments, foreign_key: 'author_id'
+  has_many :likes, foreign_key: 'author_id'
 
   def recent_posts
-    posts.limit(3).order(created_at: :desc)
+    posts.last(3)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,25 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_08_080258) do
+ActiveRecord::Schema.define(version: 2021_12_06_203810) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
     t.text "text"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.bigint "author_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
     t.bigint "post_id", null: false
+    t.bigint "author_id"
     t.index ["author_id"], name: "index_comments_on_author_id"
     t.index ["post_id"], name: "index_comments_on_post_id"
   end
 
   create_table "likes", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.bigint "author_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
     t.bigint "post_id", null: false
+    t.bigint "author_id"
     t.index ["author_id"], name: "index_likes_on_author_id"
     t.index ["post_id"], name: "index_likes_on_post_id"
   end
@@ -36,11 +37,11 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_08_080258) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "text"
-    t.integer "comment_counter"
-    t.integer "likes_counter"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.bigint "author_id", null: false
+    t.integer "comments_counter", default: 0, null: false
+    t.integer "likes_counter", default: 0, null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "author_id"
     t.index ["author_id"], name: "index_posts_on_author_id"
   end
 
@@ -48,9 +49,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_08_080258) do
     t.string "name"
     t.string "photo"
     t.text "bio"
-    t.integer "post_counter"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.integer "posts_counter", default: 0, null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   add_foreign_key "comments", "posts"
